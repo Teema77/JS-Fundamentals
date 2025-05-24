6f9d9cd62d5d7a2b5fe1d1864f46c692a02b230e
Fatima Ramoni <olabisifat@gmail.com>
2025-05-24 09:24:57 +0100

    Add a script that prints 3 lines

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,16 @@
-const message = ["C is fun", "Python is cool", "JavaScript is amazing"];
+/*const message = ["C is fun", "Python is cool", "JavaScript is amazing"];
 for(let i=0; i<message.length; i++) {
     console.log(message[i]);
+}*/
+
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let output = "";
+
+for (let i = 0; i < messages.length; i++) {
+  output += messages[i];
+  if (i !== messages.length - 1) {
+    output += "\n";
+  }
 }
+
+console.log(output);
