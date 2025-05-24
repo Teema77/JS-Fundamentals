94afe78c392f9af94fbb9f600fece8e6431a1a3f
Fatima Ramoni <olabisifat@gmail.com>
2025-05-24 16:53:54 +0100

    Add scripts that has 3 lines

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,14 @@
-const message = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for(let i=0; i<message.length; i++) {
-    console.log(message[i]);
+/*const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for(let i = 0; i < messages.length; i++) {
+    console.log(messages[i]);
+}*/
+
+
+
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let i = 0;
+
+while (i < messages.length) {
+  console.log(messages[i]);
+  i++;
 }
